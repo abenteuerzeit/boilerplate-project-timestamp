76f80cfc6676d449d423c04513f545babb33b803
Replit user <>
2023-10-29 07:25:03 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
-// index.js
-// where your node app starts
+const express = require("express");
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
+const cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(express.static("public"));
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+/**
+ * Endpoint: "/api/:date?"
+ *
+ * Description:
+ * This endpoint provides the Unix timestamp and UTC string representation
+ * of a given date. It can handle both date strings and Unix timestamps as input.
+ *
+ * Usage:
+ * 1. Without a date parameter:
+ *    - URL: /api/
+ *    - Returns the current time as a Unix timestamp and UTC string.
+ *
+ * 2. With a date string:
+ *    - URL: /api/YYYY-MM-DD (e.g., /api/2023-01-01)
+ *    - Returns the given date's Unix timestamp and UTC string.
+ *
+ * 3. With a Unix timestamp:
+ *    - URL: /api/TIMESTAMP (e.g., /api/1641014400000)
+ *    - Returns the date corresponding to the Unix timestamp in both Unix and UTC formats.
+ *
+ * Errors:
+ * If an invalid date or timestamp is provided, the endpoint returns an error
+ * message in the format: { error : "Invalid Date" }
+ */
+app.get("/api/:date?", (req, res) => {
+  const dateStr = req.params.date;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  if (!dateStr) {
+    return res.json({
+      unix: Date.now(),
+      utc: new Date().toUTCString(),
+    });
+  }
+
+  const parsedDate = /^\d+$/.test(dateStr)
+    ? new Date(parseInt(dateStr))
+    : new Date(dateStr);
+
+  return parsedDate.toString() === "Invalid Date"
+    ? res.json({ error: "Invalid Date" })
+    : res.json({
+      unix: parsedDate.getTime(),
+      utc: parsedDate.toUTCString(),
+    });
 });
 
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT, function () {
+  console.log("App listening on port " + listener.address().port);
 });
